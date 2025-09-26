8e714b9df493cdf02e028c14d15e481054895efd
Pedro Ferraz <pvf@cesar.school>
2025-09-26 14:54:34 -0300

    DAOS

--- src/br/edu/cs/poo/ac/ordem/daos/FechamentoOrdemServicoDAO.java
+++ src/br/edu/cs/poo/ac/ordem/daos/FechamentoOrdemServicoDAO.java
@@ -1,5 +1,55 @@
 package br.edu.cs.poo.ac.ordem.daos;
 
-public class FechamentoOrdemServicoDAO {
+import java.io.Serializable;
 
+import br.edu.cs.poo.ac.ordem.entidades.FechamentoOrdemServico;
+
+public class FechamentoOrdemServicoDAO extends DAOGenerico {
+
+	 public FechamentoOrdemServicoDAO() {
+	        super(FechamentoOrdemServico.class);
+	    }
+
+	    public FechamentoOrdemServico buscar(String numeroOrdem) {
+	        return (FechamentoOrdemServico) cadastroObjetos.buscar(numeroOrdem);
+	    }
+
+	    public boolean incluir(FechamentoOrdemServico fechamento) {
+	        if (buscar(fechamento.getNumeroOrdemServico()) == null) {
+	            cadastroObjetos.incluir(fechamento, fechamento.getNumeroOrdemServico());
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    public boolean alterar(FechamentoOrdemServico fechamento) {
+	        if (buscar(fechamento.getNumeroOrdemServico()) != null) {
+	            cadastroObjetos.alterar(fechamento, fechamento.getNumeroOrdemServico());
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    public boolean excluir(String numeroOrdem) {
+	        if (buscar(numeroOrdem) != null) {
+	            cadastroObjetos.excluir(numeroOrdem);
+	            return true;
+	        }
+	        return false;
+	    }
+
+	    public FechamentoOrdemServico[] buscarTodos() {
+	        Serializable[] ret = cadastroObjetos.buscarTodos();
+	        FechamentoOrdemServico[] retorno;
+
+	        if (ret != null && ret.length > 0) {
+	            retorno = new FechamentoOrdemServico[ret.length];
+	            for (int i = 0; i < ret.length; i++) {
+	                retorno[i] = (FechamentoOrdemServico) ret[i];
+	            }
+	        } else {
+	            retorno = new FechamentoOrdemServico[0];
+	        }
+	        return retorno;
+	    }
 }
--- src/br/edu/cs/poo/ac/ordem/daos/NotebookDAO.java
+++ src/br/edu/cs/poo/ac/ordem/daos/NotebookDAO.java
@@ -1,5 +1,63 @@
 package br.edu.cs.poo.ac.ordem.daos;
 
-public class NotebookDAO {
+import java.io.Serializable;
 
+import br.edu.cs.poo.ac.ordem.entidades.Notebook;
+
+public class NotebookDAO extends DAOGenerico {
+
+	public NotebookDAO() {
+		super(Notebook.class);
+	}
+
+	private String getCodigo(Notebook Notebook) {
+
+		return Notebook.getIdTipo() + Notebook.getSerial();
+	}
+
+	public Notebook buscar(String codigo) {
+		return (Notebook) cadastroObjetos.buscar(codigo);
+	}
+
+	public boolean incluir(Notebook Notebook) {
+		if (buscar(getCodigo(Notebook)) == null) {
+
+			cadastroObjetos.incluir(Notebook, getCodigo(Notebook));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean alterar(Notebook Notebook) {
+		if (buscar(getCodigo(Notebook)) != null) {
+			cadastroObjetos.alterar(Notebook, getCodigo(Notebook));
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean excluir(String codigo) {
+		if (buscar(codigo) != null) {
+			cadastroObjetos.excluir(codigo);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Notebook[] buscarTodos() {
+		Serializable[] ret = cadastroObjetos.buscarTodos();
+		Notebook[] retorno;
+		if (ret != null && ret.length > 0) {
+			retorno = new Notebook[ret.length];
+			for (int i = 0; i < ret.length; i++) {
+				retorno[i] = (Notebook) ret[i];
+			}
+		} else {
+			retorno = new Notebook[0];
+		}
+		return retorno;
+	}
 }
